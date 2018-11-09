e4112160306689aa702c4c759149abad1e941ca4
dKab <dkabardinov@gmail.com>
2018-11-09 19:41:24 +0300

    Add router and axios for http requests

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import AppRouter from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LoginForm from './containers/login/login';
 
-ReactDOM.render(<LoginForm></LoginForm>, document.getElementById('root'));
+ReactDOM.render(<AppRouter></AppRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
