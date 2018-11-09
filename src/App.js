d98de4c81feb13f3f24c90d89cc6a72de4112bbc
dKab <dkabardinov@gmail.com>
2018-11-10 00:10:18 +0300

    make tasksView home component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoginForm from './containers/login/login';
 import {PrivateRoute} from './utils';
-import Home from './containers/home';
 import SignUpForm from './containers/signup/signup';
 import { AuthButton } from "./components/auth-button";
 import TaskView from "./containers/taskview/taskview";
@@ -13,8 +12,7 @@ const AppRouter = () => (
         <AuthButton />
         <Route path="/login" exact component={LoginForm} />
         <Route path='/signup' exact component={SignUpForm} />
-        <PrivateRoute path='/' exact component={Home}></PrivateRoute>
-        <Route path='/tasks' component={TaskView}></Route>
+        <PrivateRoute path='/' exact component={TaskView}></PrivateRoute>
       </div>
   </Router>
 );
